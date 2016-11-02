693ed7d6444c8d485c3c8112f7d2df76041b88f9
Alejandro Del sanz Masia <aldema@floridauniversitaria.es>
2016-11-02 11:52:40 +0100

    hola

---
+++ while.php
@@ -0,0 +1,29 @@
+
+
+
+//Probar estructura while
+<!DOCTYPE html>
+<html>
+  <head>
+	<title> Actividad 7 </title>
+ </head>
+<body>
+	<h1> Tabla de multiplicar 2</h1>
+	<?php
+
+
+		$n=1;
+
+
+		while ($n <= 10) {
+			$mutiplicar= $n * 2;
+			echo " 2 * $n = $mutiplicar <br>";
+			$n++;
+		}
+
+
+
+
+	 ?>
+      </body>
+   </html>
